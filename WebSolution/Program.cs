55e775128dd67988843b34e43fc41e70ac3cd1bd
ChaudhryAsif <ma232505@gmail.com>
2023-01-31 10:51:07 +0500

    5 - Data context

--- WebSolution/Program.cs
+++ WebSolution/Program.cs
@@ -1,7 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using WebSolution.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+//{
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+//    options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+//});
 
 var app = builder.Build();
 
